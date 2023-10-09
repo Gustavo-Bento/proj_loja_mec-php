cf5b7039fd4f3aa1fb17d7d9ca68eaa93001988f
luide166 <luide166@gmail.com>
2023-10-09 19:51:58 -0300

    Iniciando projeto da loja mec

---
+++ cabecalho.php
@@ -0,0 +1,16 @@
+<div id="cabecalho_superior">
+    <nav id="menu-cima">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+</div>
+<div id="cabecalho_meio">
+
+</div>
+<div id="cabecalho_inferior">
+
+</div>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja MEC | Tudo em eletrodoméstico</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+    <div id="Principal">
+        <div id="topo">
+            <?php include_once("cabecalho.php") ?>
+        </div>
+        <div id="corpo">
+
+        </div>
+        <div id="rodape">
+
+        </div>
+    </div>
+</body>
+
+</html>
