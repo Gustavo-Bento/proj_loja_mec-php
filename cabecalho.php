3c69e6147b65f6f40c35b50726e7b175756edae8
luide166 <luide166@gmail.com>
2023-10-11 19:31:55 -0300

    Formatando o cabeçalho da loja virtual

--- cabecalho.php
+++ cabecalho.php
@@ -1,16 +1,33 @@
 <div id="cabecalho_superior">
     <nav id="menu-cima">
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="#">Minha conta</a></li>
+            <li><a href="#">Meu carrinho</a></li>
+            <li><a href="#">Lugar</a></li>
+            <li><a href="#">Cadastrar</a></li>
         </ul>
     </nav>
 </div>
 <div id="cabecalho_meio">
-
+    <h1>Lojas MEC - os melhores eletrodomesticos estão aqui</h1>
+    <p>Nenhum item no seu carrinho de compras</p>
+    <section>
+        <form action="">
+            <label>
+                <span>Busca</span>
+                <input type="search" name="pesquisa" id="pesquisa">
+                <input type="image" src="img/lupa.png">
+            </label>
+        </form>
+    </section>
 </div>
 <div id="cabecalho_inferior">
-
+    <nav id="menu-principal">
+        <ul>
+            <li><a href="#">Nome</a></li>
+            <li><a href="#">Produtos</a></li>
+            <li><a href="#">Fale conosco</a></li>
+            <li><a href="#">Quem somos</a></li>
+        </ul>
+    </nav>
 </div>
